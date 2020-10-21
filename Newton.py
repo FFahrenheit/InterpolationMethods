bc68d2fdc5ade249c08f25467ec27bf6bf329706
Iván L. Murillo <diskman199@gmail.com>
2020-10-21 13:05:14 -0500

    Cambios con epsilon

--- Newton.py
+++ Newton.py
@@ -1,7 +1,24 @@
+""""Use requestData method to fill with your own data
+but I recommend to fill it statically in the array"""
+
+epsilon = 4
+
 def main():
     print("\t\tNewton con diferencias divididas\n\n")
-    x = 2
-    data = [[1,56.5],[5,113.0],[20,181.0],[40,214.5]]
+    
+    #x = 2
+    #data = [[1,56.5],[5,113.0],[20,181.0],[40,214.5]]
+
+    #        Xn    f(Xn)
+    data = [[20,17.535],
+           [40,55.324],
+           [60,149.380],
+           [80,355.110]]
+
+	#   Valor a encontrar
+    x = 30
+    #x = 2
+    #data = [[1,56.5],[5,113.0],[20,181.0],[40,214.5]]
 
     n = len(data)
     polynomials = n-1
@@ -17,7 +34,7 @@ def main():
             if j != 0:
                 print(" + ",end='')
             total += getMultiplication(data,j,x) * getDividedDifference(dif,k,j)
-        total = round(total,4)
+        total = round(total,epsilon)
         print(" = %.4f\n"%total)
 
 """
@@ -57,9 +74,9 @@ def calculateDividedDiferences(data):
     for i in range(n):   #Repeat n-1 times because we already have a0 
         diff = i+1 #First iteration we'll need n-1 values, but second time we'll need n-2 rows
         for j in range(n-diff): 
-            print("(%.4f-%.4f)/(%f - %f)" % (dif[j+1][i],dif[j][i],data[j+diff][0],data[j][0]))
+            print("(%.4f-%.4f)/(%d - %d)" % (dif[j+1][i],dif[j][i],data[j+diff][0],data[j][0]))
             ak = (dif[j+1][i] - dif[j][i])/(data[j+diff][0]-data[j][0]) #a[k] calculation 
-            ak = round(ak,4)
+            ak = round(ak,epsilon)
             print("a[%d][%d] = %.4f\n"%(i,j,ak))
             dif[j][i+1] = ak
     printMatrix(dif) #Print the resultant matrix
